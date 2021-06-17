9455202e13ef8d5f5d92eb6b45541aaefbc44db5
EC2 Default User <ec2-user@ip-172-31-69-209.ec2.internal>
2021-06-17 07:17:34 +0000

    [add]member_model

--- db/schema.rb
+++ db/schema.rb
@@ -35,13 +35,21 @@ ActiveRecord::Schema.define(version: 20210617022400) do
   end
 
   create_table "members", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "name_sei",               default: "",    null: false
+    t.string   "name_mei",               default: "",    null: false
+    t.string   "name_kana_sei",          default: "",    null: false
+    t.string   "name_kana_mei",          default: "",    null: false
+    t.string   "postal_code",            default: "",    null: false
+    t.string   "address",                default: "",    null: false
+    t.string   "tel",                    default: "",    null: false
+    t.boolean  "taikai_status",          default: false, null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
