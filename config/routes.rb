18da2a9267ee89275f6f099e92344a6bdd13e342
EC2 Default User <ec2-user@ip-172-31-23-157.ec2.internal>
2021-06-18 17:29:58 +0000

    注文情報入力画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :members
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :member do
+    resources :orders, only: [:new, :create, :index, :show] do
+      get :confirm, on: :collection
+      get :thanks, on: :collection
+    end
+  end
+
 end
