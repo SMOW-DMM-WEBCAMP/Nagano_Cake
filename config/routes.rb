c345da9a309c3217d9bc9319bb59bd544d959f36
EC2 Default User <ec2-user@ip-172-31-12-141.ec2.internal>
2021-06-19 08:21:28 +0000

    新規商品登録画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :members
+  namespace :admin do
+  resources :products,only: [:new,:create,:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
