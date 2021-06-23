0abd119faef6c36b740188a821cf7c2874d755c6
mo3310 <str.ftg0790@gmail.com>
2021-06-23 07:56:24 +0000

    [update]new.html/form_with

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name_sei, :name_mei, :name_kana_sei, :name_kana_mei, :postal_code, :address, :tell])
   end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -8,14 +8,14 @@ class Member < ApplicationRecord
   has_many :orders
   has_many :shipping_addresses
 
-  validates :name_sei, :name_mei, :name_kana_sei, :name_kana_mei, :postal_code, :address, :tel,
-            presence: true
-  validates :postal_code, length: {is: 7}, numericality: { only_integer: true }
-  validates :tel, numericality: { only_integer: true }
-  VALID_PHONE_NUMBER_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
-  validates :tel, presence: true, format: { with: VALID_PHONE_NUMBER_REGEX }
-  validates :name_kana_sei, :name_kana_mei,
-    format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "カタカナで入力して下さい。" }
+  validates :name_sei, :name_mei, :name_kana_sei, :name_kana_mei, :postal_code, :address, :tel, presence: true
+  validates :name_kana_sei, :name_kana_mei, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "カタカナで入力して下さい。" }
+
+  VALID_TEL_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
+  validates :tel, numericality: { only_integer: true }, format: { with: VALID_TEL_REGEX }
+
+  VALID_POSTAL_CODE_REGEX = /\A\d{7}\z/
+  validates :postal_code, length: {is: 7}, numericality: { only_integer: true }, format: { with: VALID_POSTAL_CODE_REGEX }
 
   # 退会機能
   def active_for_authentication?
