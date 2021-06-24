e4d27ffaf72ae3118e0ffbce2cb017dd52fde356
mo3310 <str.ftg0790@gmail.com>
2021-06-24 09:17:08 +0000

    [add]新規登録とログイン確認

--- app/models/member.rb
+++ app/models/member.rb
@@ -11,15 +11,14 @@ class Member < ApplicationRecord
   validates :name_sei, :name_mei, :name_kana_sei, :name_kana_mei, :postal_code, :address, :tel, presence: true
   validates :name_kana_sei, :name_kana_mei, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: "カタカナで入力して下さい。" }
 
-  VALID_TEL_REGEX = /\A0(\d{1}[-(]?\d{4}|\d{2}[-(]?\d{3}|\d{3}[-(]?\d{2}|\d{4}[-(]?\d{1})[-)]?\d{4}\z|\A0[5789]0[-]?\d{4}[-]?\d{4}\z/
-  validates :tel, numericality: { only_integer: true }, format: { with: VALID_TEL_REGEX }
+  validates :tel, format: {with: /\A[0-9]{3}[0-9]{4}[0-9]{4}\z/}
 
   VALID_POSTAL_CODE_REGEX = /\A\d{7}\z/
   validates :postal_code, length: {is: 7}, numericality: { only_integer: true }, format: { with: VALID_POSTAL_CODE_REGEX }
 
   # 退会機能
   def active_for_authentication?
-    super && (self.is_valid == false)
+    super && (self.taikai_status == false)
   end
 
 end
