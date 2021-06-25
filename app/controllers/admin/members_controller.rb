213a785a756b9bdf90328358d9f27d6026432a09
mo3310 <str.ftg0790@gmail.com>
2021-06-25 06:07:09 +0000

    edit,show作成途中

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,2 +1,32 @@
 class Admin::MembersController < ApplicationController
+
+  before_action :authenticate_admin!
+
+  def index
+    @members = Member.all.page(params[:page]).per(10)
+  end
+
+	def show
+  	@member = Member.find(params[:id])
+	end
+
+	def edit
+  	@member = Member.find(params[:id])
+	end
+
+	def update
+  	@member = Member.find(params[:id])
+		if @member.update(customer_params)
+		   flash[:success] = "会員情報を変更しました"
+		   redirect_to admin_member_path
+		else
+			render "edit"
+		end
+	end
+
+	private
+	def member_params
+	  params.require(:member).permit(:name_sei,:name_mei,:name_kana_sei,:name_kana_mei,:postal_code,:address,:tel,:email,:taikai_status)
+	end
+
 end
--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -1,5 +1,18 @@
 class Member::MembersController < ApplicationController
 
+  def show
+    @member = Member.find(params[:id])
+  end
+
+  def edit
+    @member = Member.find(params[:id])
+    if @member == current_user
+      render "edit"
+    else
+      redirect_to member_path(current_user)
+    end
+  end
+
   def out
     member = current_member
     member.update(is_deleted: true)
@@ -7,6 +20,5 @@ class Member::MembersController < ApplicationController
     flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
     redirect_to root_path
   end
-
 
 end
