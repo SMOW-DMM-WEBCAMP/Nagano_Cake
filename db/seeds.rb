701f22ee20022dbdbb59142933052c839b134312
EC2 Default User <ec2-user@ip-172-31-93-133.ec2.internal>
2021-06-27 10:06:46 +0000

    [alter]cart

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 Member.create(name_sei: '田中', name_mei: '太郎', name_kana_sei: 'タナカ', name_kana_mei: 'タロウ', postal_code: '1111111', address: '東京都新宿区1-1', tel: '11111111111', email: 'tanaka@test.com', password: '000000')
 Member.create(name_sei: '山田', name_mei: '次郎', name_kana_sei: 'ヤマダ', name_kana_mei: 'ジロウ', postal_code: '2222222', address: '東京都新宿区2-2', tel: '22222222222', email: 'yamada@test.com', password: '000000')
 
-#Admin.create!(
+Admin.create!(
   #email: 'admin@admin.com',
   #password: '000000',
 #)
